d4ee80ed5c29f86513774a938f5fcc1ba337a256
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-06 23:32:56 +0700

    Test API responses via Postman

--- src/main/java/vn/tunguyen/jobhunter/controller/UserController.java
+++ src/main/java/vn/tunguyen/jobhunter/controller/UserController.java
@@ -4,8 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.tunguyen.jobhunter.domain.User;
 import vn.tunguyen.jobhunter.service.UserService;
-
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -14,14 +19,29 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping("/")
-    public String getMethodName() {
-        User user = new User();
-        user.setName("Tunguyen");
-        user.setEmail("tunguyen@gmail.com");
-        user.setPassword("123456");
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+        return this.userService.fetchUserById(id);
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return this.userService.fetchAllUser();
+    }
+    @PostMapping("/user")
+    public String createNewUser(@RequestBody User user) {
         this.userService.handleCreateUser(user);
         return "TuNguyen";
     }
-    
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "TuNguyen";
+    }
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        User newuser = this.userService.handleUpdateUser(user);
+        return newuser;
+    }
 }
--- src/main/java/vn/tunguyen/jobhunter/service/UserService.java
+++ src/main/java/vn/tunguyen/jobhunter/service/UserService.java
@@ -1,6 +1,8 @@
 package vn.tunguyen.jobhunter.service;
 
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 import vn.tunguyen.jobhunter.domain.User;
 import vn.tunguyen.jobhunter.repository.UserRepository;
@@ -11,7 +13,31 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    public User fetchUserById(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+    public List<User> fetchAllUser() {
+        return this.userRepository.findAll();
+    }
+    public User handleUpdateUser(User reqUser) {
+        User currentUser = this.fetchUserById(reqUser.getId());
+        if (currentUser != null) {
+            currentUser.setEmail(reqUser.getEmail());
+            currentUser.setName(reqUser.getName());
+            currentUser.setPassword(reqUser.getPassword());
+
+            currentUser = this.userRepository.save(currentUser);
+        }
+        return currentUser;
+    }
     public void handleCreateUser(User user) {
         this.userRepository.save(user);
+    }
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
     }
 }
