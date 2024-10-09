f1af3e8df000c3466c69954dc5ae2853479e3220
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-09 22:16:10 +0700

    Move exception handling to global scope with @RestControllerAdvice

--- src/main/java/vn/tunguyen/jobhunter/controller/UserController.java
+++ src/main/java/vn/tunguyen/jobhunter/controller/UserController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,10 +37,6 @@ public class UserController {
         return ResponseEntity.ok("TuNguyen");
     }
 
-    @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdException(IdInvalidException idException) {
-        return ResponseEntity.badRequest().body(idException.getMessage());
-    }
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable("id") long id) throws IdInvalidException {
         if (id >= 1500) {
