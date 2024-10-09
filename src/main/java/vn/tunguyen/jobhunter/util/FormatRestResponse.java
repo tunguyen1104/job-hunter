6678debf2b2ac8ad685a2e07fa29a43582d95a14
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-10 00:24:56 +0700

    Add FormatRestResponse class for global response formatting

---
+++ src/main/java/vn/tunguyen/jobhunter/util/FormatRestResponse.java
@@ -0,0 +1,47 @@
+package vn.tunguyen.jobhunter.util;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import jakarta.servlet.http.HttpServletResponse;
+import vn.tunguyen.jobhunter.domain.RestResponse;
+
+@ControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response) {
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+
+        RestResponse<Object> result = new RestResponse<Object>();
+        result.setStatusCode(status);
+
+        if (status >= 400) {
+            return body;
+        } else {
+            result.setData(body);
+            result.setMessage("CALL API SUCCESS");
+        }
+
+        return result;
+    }
+
+}
