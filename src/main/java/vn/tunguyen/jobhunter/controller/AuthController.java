ca9eca4246a76d8593550c93a61354f742d13832
TuNguyen1104 <tu.pro5564@gmail.com>
2024-12-25 13:51:04 +0700

    feat: login endpoint return JWT token

--- src/main/java/vn/tunguyen/jobhunter/controller/AuthController.java
+++ src/main/java/vn/tunguyen/jobhunter/controller/AuthController.java
@@ -10,24 +10,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.tunguyen.jobhunter.domain.dto.LoginDto;
+import vn.tunguyen.jobhunter.domain.dto.ResLoginDto;
+import vn.tunguyen.jobhunter.util.SecurityUtil;
 
 @RestController
 public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SecurityUtil securityUtil;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder,
+            SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityUtil = securityUtil;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDto> login(@Valid @RequestBody LoginDto loginDto) {
+    public ResponseEntity<ResLoginDto> login(@Valid @RequestBody LoginDto loginDto) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword());
 
         // xác thực người dùng => cần viết hàm loadUserByUsername
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return ResponseEntity.ok().body(loginDto);
+        String access_token = this.securityUtil.createToken(authentication);
+        ResLoginDto res = new ResLoginDto();
+        res.setAccessToken(access_token);
+        return ResponseEntity.ok().body(res);
     }
 }
---
+++ src/main/java/vn/tunguyen/jobhunter/domain/dto/ResLoginDto.java
@@ -0,0 +1,14 @@
+package vn.tunguyen.jobhunter.domain.dto;
+
+public class ResLoginDto {
+    private String accessToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
+    
