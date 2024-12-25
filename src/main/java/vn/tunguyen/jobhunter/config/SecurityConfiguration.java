1f4c4d7de8ebba474ff0457662f186d5765b39ca
TuNguyen1104 <tu.pro5564@gmail.com>
2024-12-25 13:46:36 +0700

    feat: Add SecurityUtil for JWT token creation and management

--- src/main/java/vn/tunguyen/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/tunguyen/jobhunter/config/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package vn.tunguyen.jobhunter.config;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,11 +11,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+
+import vn.tunguyen.jobhunter.util.SecurityUtil;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
+
+    @Value("${tunguyen.jwt.base64-secret}")
+    private String jwtKey;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,5 +46,16 @@ public class SecurityConfiguration {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length,
+                SecurityUtil.JWT_ALGORITHM.getName());
     }
 }
