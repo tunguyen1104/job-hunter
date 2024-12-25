97f0f110ce559b3c88b1749c404e34963567d548
TuNguyen1104 <tu.pro5564@gmail.com>
2024-12-25 23:17:40 +0700

    feat: Custom JWT Authentication Entry Point

--- src/main/java/vn/tunguyen/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/tunguyen/jobhunter/config/SecurityConfiguration.java
@@ -16,6 +16,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
@@ -36,13 +38,17 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http
             .csrf(c -> c.disable())
             .authorizeHttpRequests(authz -> authz
                 .requestMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated())
-            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
+                .authenticationEntryPoint(customAuthenticationEntryPoint))
+            // .exceptionHandling(exceptions -> exceptions
+            //     .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) // 401
+            //     .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) // 403
             .formLogin(f -> f.disable())
             .sessionManagement(session -> 
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/java/vn/tunguyen/jobhunter/util/error/GlobalException.java
+++ src/main/java/vn/tunguyen/jobhunter/util/error/GlobalException.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,11 +17,11 @@ import vn.tunguyen.jobhunter.domain.RestResponse;
 
 @RestControllerAdvice
 public class GlobalException {
-    @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idException) {
+    @ExceptionHandler(value = {UsernameNotFoundException.class, BadCredentialsException.class})
+    public ResponseEntity<RestResponse<Object>> handleIdException(Exception ex) {
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.BAD_REQUEST.value());
-        res.setError(idException.getMessage());
+        res.setError(ex.getMessage());
         res.setMessage("IdInvalidException");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
