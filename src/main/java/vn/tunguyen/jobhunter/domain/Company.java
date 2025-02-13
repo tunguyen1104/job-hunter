99d883af99faecf62d67b6457820331b11e47351
TuNguyen1104 <tu.pro5564@gmail.com>
2025-02-13 23:52:54 +0700

    feat(domain): add Company entity with basic fields

---
+++ src/main/java/vn/tunguyen/jobhunter/domain/Company.java
@@ -0,0 +1,39 @@
+package vn.tunguyen.jobhunter.domain;
+
+import java.time.Instant;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "companies")
+@Entity
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = "name is not blank")
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    private String address;
+
+    private String logo;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+}
