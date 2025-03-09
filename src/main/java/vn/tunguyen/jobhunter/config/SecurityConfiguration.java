e2c273d2bdd6f0689c0daea19e7d70e61a418897
tunguyen1104 <tu.pro5564@gmail.com>
2025-03-09 23:05:53 +0700

    fix: update requestMatchers for /, /api/v1/login in security configuration

--- src/main/java/vn/tunguyen/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/tunguyen/jobhunter/config/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration {
             .csrf(c -> c.disable())
             .cors(Customizer.withDefaults())
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/", "/login").permitAll()
+                .requestMatchers("/", "/api/v1/login").permitAll()
                 .anyRequest().authenticated())
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                 .authenticationEntryPoint(customAuthenticationEntryPoint))
