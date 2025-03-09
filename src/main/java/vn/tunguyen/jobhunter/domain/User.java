20c71f65cf8f2524ca7f1286e40e6f2a4c350992
tunguyen1104 <tu.pro5564@gmail.com>
2025-03-09 10:15:56 +0700

    feat: Add auditing and integrate Lombok in User entity

--- src/main/java/vn/tunguyen/jobhunter/domain/User.java
+++ src/main/java/vn/tunguyen/jobhunter/domain/User.java
@@ -2,16 +2,29 @@ package vn.tunguyen.jobhunter.domain;
 
 import java.time.Instant;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import vn.tunguyen.jobhunter.util.SecurityUtil;
 import vn.tunguyen.jobhunter.util.constant.GenderEnum;
 
+@Getter
+@Setter
+@Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 public class User {
     @Id
@@ -31,36 +44,21 @@ public class User {
     private String createdBy;
     private String updatedBy;
 
-    public String getName() {
-        return name;
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+
+        this.createdAt = Instant.now();
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
 
-    public String getEmail() {
-        return email;
+        this.updatedAt = Instant.now();
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
 }
